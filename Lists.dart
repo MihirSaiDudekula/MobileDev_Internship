4781f085c899d2b6669633d525e8add85f5769f6
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2023-10-26 14:54:24 +0530

    Create Lists.dart

---
+++ Lists.dart
@@ -0,0 +1,10 @@
+void main() {
+  List marks=[10,20,30];
+  int length=marks.length;
+  print(length);
+  marks.remove(0);
+  print(marks);
+  List<int>scores=[100,90,80];
+  print(marks[2]); 
+  print("$scores");
+}
