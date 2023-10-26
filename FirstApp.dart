9097a6d8075ec7e2a4083927233c9fcd1cc07d2a
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2023-10-26 15:44:08 +0530

    Update FirstApp.dart

--- FirstApp.dart
+++ FirstApp.dart
@@ -1,44 +1,42 @@
 import 'package:flutter/material.dart';
-
-void main() {
+void main(){
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
       home:Scaffold(
-        appBar: AppBar(
-              title: const Text('Rich text example'),
+        appBar:AppBar(  
+          title:const Text('RichText Example'),
         ),
-      ),
-      body:Center(
-        child:RichText(
-          text: TextSpan(
-            text: 'Hello',
-            style: DefaultTextStyle.of(context).style,
-            children: const<TextSpan>[
-              TextSpan(
-                text: 'bold',
-                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue)
+        body:Center(  
+          child:RichText(
+            text:TextSpan(  
+            text:'hello',
+            style:DefaultTextStyle.of(context).style,
+            children:const <TextSpan>[
+              TextSpan(  
+                text:'bold',
+                style:TextStyle(fontWeight: FontWeight.bold,color: Colors.blue),
               ),
               TextSpan(
-                text: 'world',
-                style: TextStyle(fontWeight: FontWeight.italic, color: Colors.green)
+                text:'world!',
+                style:TextStyle(fontStyle: FontStyle.italic,color:Colors.green),
               ),
               TextSpan(
-                text: 'this is a',
-                style: TextStyle(color: Colors.black)
+                text:'this is a',
+                style:TextStyle(color:Colors.black),
               ),
-              TextSpan(
-                text: 'different color',
-                style: TextStyle(color: Colors.red)
+              TextSpan(  
+                text:'different color',
+                style:TextStyle(color:Colors.red),
               ),
-            ] ),)
+            ])
+          )
+        )
       )
+    );
   }
 }
