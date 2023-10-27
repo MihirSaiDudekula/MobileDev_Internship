3431030518bae183d139371da1f4e0f886c44679
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2023-10-27 14:38:12 +0530

    Create 11argsOrder.dart

---
+++ Basic Programs in Dart/11argsOrder.dart
@@ -0,0 +1,10 @@
+void printInfo(String name,String gender)
+{
+  print("Hello $name, your gender is $gender");
+}
+
+void main() {
+  // arguments order is important
+  printInfo("John","male");
+  printInfo("female","Alice");
+ }
