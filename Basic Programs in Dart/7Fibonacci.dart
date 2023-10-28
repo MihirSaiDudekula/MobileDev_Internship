d91fae8e9984566227227fe8da5a709a223d6c4d
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2023-10-28 12:35:25 +0530

    Update 7Fibonacci.dart

--- Basic Programs in Dart/7Fibonacci.dart
+++ Basic Programs in Dart/7Fibonacci.dart
@@ -1,12 +1,12 @@
-void main(){
-  List a=[];
-  a[0]=1;
-  a[1]=1;
-  int sum=0;
-  for(int i=2;i<12;i++)
-  {
-    sum=(a[i-1]+a[i-2]);
-    a.add(sum);
+void main() {
+  int n1=0,n2=1,n3;
+  print(n1);
+  print(n2);
+  
+  for(int i = 2 ;i <= 16; i++){
+   n3 = n1+n2;
+    print('$n3');
+    n1 = n2;
+    n2 = n3;
   }
-  print(a);
 }
