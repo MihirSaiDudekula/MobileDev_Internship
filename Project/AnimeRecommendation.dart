5fd65cf0526eb646fbe5d3fb8ae3ee065962305b
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2023-11-07 00:56:36 +0530

    Update AnimeRecommendation.dart

--- Project/AnimeRecommendation.dart
+++ Project/AnimeRecommendation.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+// this for the usage of systemoverlay style
+import 'package:flutter/services.dart';  
+
+
 
 void main() {
   runApp(Appname());
@@ -7,9 +11,12 @@ void main() {
 class Appname extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,//for removing the debug show check from the top right corner
       home: HomeScreen(),
+      
     );
   }
 }
@@ -19,15 +26,63 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+//adding function for showing account data
+void _showAccountMenu(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return SimpleDialog(
+        title: Text('Account Menu'),
+        children: <Widget>[
+          SimpleDialogOption(
+            onPressed: () {
+              Navigator.pop(context);
+              
+
+               // Close the account menu
+              
+            },
+            child: Text('Create Account'),
+          ),
+          SimpleDialogOption(
+            onPressed: () {
+              Navigator.pop(context); // Close the account menu
+              
+            },
+            child: Text('Other Option'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 class _HomeScreenState extends State<HomeScreen> {
   bool showContactInfo = false;
-
+TextEditingController nameController = TextEditingController(); // Controller for the user's name
+String Name =''; //this is for storing user name
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFEEEFF5),
       appBar: AppBar(
-        title: Text('Anime Recommendation app'),
+        backgroundColor: Color(0xFFEEEFF5),
+        elevation: 0,//shadow between appbar and body disappers
+        title: Text('Anime Recommendation app',style: TextStyle(color: Colors.red),),
+        
         centerTitle: true,//this is to center the text on the appbar
+        iconTheme: IconThemeData(color: Colors.red), // Setting the color of the drawer icon 
+          actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.account_circle,//account icon
+              color: Colors.red, // Color of the account icon
+            ),
+            onPressed: () {
+              _showAccountMenu(context);
+            },
+          ),
+        ],
       ),
       
       drawer: Drawer(
@@ -63,7 +118,7 @@ class _HomeScreenState extends State<HomeScreen> {
               title: Text('Contact Us'),
               onTap: () {
                 
-                // Navigator.pop(context);
+                Navigator.pop(context);
                 // By toggling showContactInfo from true to false or vice versa, you control whether the contact information is displayed or hidden.
                 setState(() {
                   showContactInfo = !showContactInfo;
@@ -80,3 +135,37 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+           
+    
+  
+   
+ 
