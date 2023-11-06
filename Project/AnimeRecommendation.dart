30ae9a86f7104cbae6de41cba899996c27e92c4f
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2023-11-06 15:54:12 +0530

    Update AnimeRecommendation.dart

--- Project/AnimeRecommendation.dart
+++ Project/AnimeRecommendation.dart
@@ -1,1 +1,61 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Appname());
+}
+
+class Appname extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner:false,
+      home: Scaffold(
+      
+        appBar: AppBar(
+            
+          title: Text(
+            'Anime Recommendation app',
+          ),
+          centerTitle: true,
+            
+
+            ),
+           drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
+      
+
+          
+          ),
+          // Other AppBar properties...
+        );
+      
+    
+  }
+}
