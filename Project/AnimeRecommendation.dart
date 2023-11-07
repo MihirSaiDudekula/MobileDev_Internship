4c61a5d76ce13d099e05c49a1ed749f02dcd0215
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2023-11-07 12:33:45 +0530

    Update AnimeRecommendation.dart

--- Project/AnimeRecommendation.dart
+++ Project/AnimeRecommendation.dart
@@ -1,90 +1,147 @@
 import 'package:flutter/material.dart';
-// this for the usage of systemoverlay style
-import 'package:flutter/services.dart';  
-
-
+ // this for the usage of systemoverlay style
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(Appname());
+  runApp(AppName());
 }
 
-class Appname extends StatelessWidget {
+class AppName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    SystemChrome.setSystemUIOverlayStyle(//using system overlay style 
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,//for removing the debug show check from the top right corner
       home: HomeScreen(),
-      
     );
   }
 }
-//creating statefulwidget
+//implementing the usage of the search tab
+class SearchListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: [
+          Container(
+         
+            decoration: BoxDecoration(  
+              color:Colors.white,
+              borderRadius: BorderRadius.circular(20)
+            ),
+           child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon( 
+                Icons.search,
+                color: Colors.black,
+              )
+            ),
+           ),
+            )]
+          );
+        
+      
+  }
+}
+
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-//adding function for showing account data
-void _showAccountMenu(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return SimpleDialog(
-        title: Text('Account Menu'),
-        children: <Widget>[
-          SimpleDialogOption(
-            onPressed: () {
-              Navigator.pop(context);
-              
-
-               // Close the account menu
-              
-            },
-            child: Text('Create Account'),
-          ),
-          SimpleDialogOption(
-            onPressed: () {
-              Navigator.pop(context); // Close the account menu
-              
-            },
-            child: Text('Other Option'),
-          ),
-        ],
-      );
-    },
-  );
-}
-
 class _HomeScreenState extends State<HomeScreen> {
+   int _currentIndex = 0; // initializing currentindex to 0 for the Home tab
   bool showContactInfo = false;
-TextEditingController nameController = TextEditingController(); // Controller for the user's name
-String Name =''; //this is for storing user name
+  TextEditingController createAccountController = TextEditingController();// Controller for the user's name
+  String userName = '';
+   // defining a list to switch between tab in bottom navigation bar 
+  final List<Widget> pages = [
+    
+    Container(
+      color: Colors.red,
+      child: Center(child: Text('Home Page')),
+    ),
+      SearchListScreen(), 
+    Container(
+      color: Colors.green,
+      child: Center(child: Text('Watchlist Page')),
+    ),
+  ];
+
+  void _showAccountMenu(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Create Account'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextFormField(
+                controller: createAccountController,
+                decoration: InputDecoration(labelText: 'Enter Your Name'),
+              ),
+              Row(
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      if (createAccountController.text.isNotEmpty) {
+                        setState(() {
+                          userName = createAccountController.text;
+                        });
+                      }
+                    },
+                    child: Text('Create Account'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancel'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEEEFF5),
       appBar: AppBar(
         backgroundColor: Color(0xFFEEEFF5),
-        elevation: 0,//shadow between appbar and body disappers
-        title: Text('Anime Recommendation app',style: TextStyle(color: Colors.red),),
-        
+        elevation: 0,
+        title: Text(
+          'Anime Recommendation app',
+          style: TextStyle(color: Colors.red),
+        ),
         centerTitle: true,//this is to center the text on the appbar
-        iconTheme: IconThemeData(color: Colors.red), // Setting the color of the drawer icon 
-          actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.account_circle,//account icon
-              color: Colors.red, // Color of the account icon
-            ),
-            onPressed: () {
-              _showAccountMenu(context);
-            },
+        iconTheme: IconThemeData(color: Colors.red),// Setting the color of the drawer icon 
+        actions: <Widget>[
+          Column(
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: Colors.red,// Color of the account icon
+                ),
+                onPressed: () {
+                  _showAccountMenu(context);
+                },
+              ),
+              if (userName.isNotEmpty) Text('Hello $userName',style:TextStyle(color:Colors.red)),
+            ],
           ),
         ],
       ),
-      
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -107,19 +164,18 @@ String Name =''; //this is for storing user name
               onTap: () {
                 Navigator.pop(context);// Close the drawer
               // used to navigate back to the home screen
-                Navigator.pushReplacement(   //replaces current route with new one
+                Navigator.pushReplacement(        //replaces current route with new one
                   context,
                   MaterialPageRoute(builder: (context) => HomeScreen()),
                 );
               },
             ),
             ListTile(
-              leading: Icon(Icons.contact_mail), //for creating icons
+              leading: Icon(Icons.contact_mail),//for creating icons
               title: Text('Contact Us'),
               onTap: () {
-                
                 Navigator.pop(context);
-                // By toggling showContactInfo from true to false or vice versa, you control whether the contact information is displayed or hidden.
+                // By changing showContactInfo from true to false, we can control whether the contact information is displayed or hidden.
                 setState(() {
                   showContactInfo = !showContactInfo;
                 });
@@ -132,16 +188,34 @@ String Name =''; //this is for storing user name
           ],
         ),
       ),
+       body: _currentIndex==1? SearchListScreen():Container(), //this line of code dynamically changes the body content of the Scaffold based on which tab is currently selected in the bottom navigation bar. 
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,//used for keeping track of the tabs in bottom navigation bar
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.red,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Watchlist',
+          ),
+        ]
+      )
     );
   }
 }
-
-
-
-
-
-
-
 
 
 
