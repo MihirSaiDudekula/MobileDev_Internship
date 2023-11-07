71b2364e5a2dfee8a30b41bcafac7e8654e8e65a
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2023-11-07 10:29:55 +0530

    Update flutter_assessment.dart

--- Test Solutions/flutter_assessment.dart
+++ Test Solutions/flutter_assessment.dart
@@ -31,43 +31,30 @@ class HomeScreen extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-                margin: EdgeInsets.only(right: 5), // Set a 5px right margin
-                child:ElevatedButton(
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/about_internship');
               },
               child: Text('About Internship'),
             ),
-              ),
-              Container(
-                margin: EdgeInsets.only(right: 5), // Set a 5px right margin
-                child:ElevatedButton(
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/dart_contents');
               },
               child: Text('Dart Contents'),
             ),
-              ),
-              Container(
-                margin: EdgeInsets.only(right: 5), // Set a 5px right margin
-                child:ElevatedButton(
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/flutter_contents');
               },
               child: Text('Flutter Contents'),
             ),
-              ),
-              Container(
-                margin: EdgeInsets.only(right: 5), // Set a 5px right margin
-                child:ElevatedButton(
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/feedback');
               },
               child: Text('Feedback'),
             ),
-              ),
-            
           ],
         ),
       ),
@@ -76,40 +63,133 @@ class HomeScreen extends StatelessWidget {
 }
 
 // Rest of the classes go here...
-class ElementCard extends StatelessWidget {
-  final String title;
-  final VoidCallback onTap;
 
-  ElementCard(this.title, this.onTap);
-
+class FeedbackPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: InkWell(
-        onTap: onTap,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Feedback"),
+      ),
+
+      body: SingleChildScrollView(
+  child: Container(
+    color: Colors.blue, // Set background color to blue
+    padding: EdgeInsets.all(20.0),
+    child: Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Feedback',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Please write your feedback below',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
             children: <Widget>[
+              RadioOption('Bad'),
+              RadioOption('Average'),
+              RadioOption('Good'),
+            ],
+          ),
+          SizedBox(height: 10),
+          InputField(label: 'Full Name', hintText: 'Enter your full name'),
+          InputField(label: 'Age', hintText: 'Enter your age'),
+          InputField(label: 'Email', hintText: 'Enter your email'),
+          InputField(label: 'Phone', hintText: 'Enter your phone number'),
+          SizedBox(height: 10),
+          Text(
+            'Message',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          TextFormField(
+            maxLines: 5,
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              Checkbox(value: false, onChanged: null),
               Text(
-                title,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+                'I agree to the Terms and Conditions',
+                style: TextStyle(color: Colors.white),
               ),
             ],
           ),
-        ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Add code to submit feedback
+            },
+            child: Text('Submit'),
+          ),
+        ],
       ),
+    ),
+  ),
+),
+//   ),
+// ),
+
     );
   }
 }
 
+class RadioOption extends StatelessWidget {
+  final String optionText;
+
+  RadioOption(this.optionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Radio(value: optionText, groupValue: null, onChanged: null),
+        Text(optionText, style: TextStyle(color: Colors.white)),
+      ],
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  final String label;
+  final String hintText;
+
+  InputField({required this.label, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label, style: TextStyle(color: Colors.white)),
+        TextFormField(
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            hintText: hintText,
+          ),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+}
 
 class AboutInternshipPage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -129,12 +209,12 @@ class AboutInternshipPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             Text(
-                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 
-                    style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                ),
-             ),
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -162,8 +242,6 @@ class AboutInternshipPage extends StatelessWidget {
     );
   }
 }
-
-
 
 class DartContentsPage extends StatelessWidget {
   @override
@@ -199,7 +277,7 @@ class FlutterContentsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contents of Dart"),
+        title: Text("Contents of Flutter"),
       ),
       body: Center(
         child: Column(
@@ -219,123 +297,6 @@ class FlutterContentsPage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class FeedbackPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Feedback"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Feedback",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Divider(height: 30, color: Colors.black),
-            Text("Please write your feedback below"),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("How do you rate your overall experience?"),
-            ),
-            Row(
-              children: [
-                FeedbackOption("Bad"),
-                FeedbackOption("Average"),
-                FeedbackOption("Good"),
-              ],
-            ),
-            
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Full name*"),
-                      TextFormField(),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Email*"),
-                      TextFormField(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Age*"),
-                      TextFormField(),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Phone*"),
-                      TextFormField(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Message*"),
-            ),
-            TextFormField(
-              maxLines: 5,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Add code to submit feedback
-              },
-              child: Text("Submit"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class FeedbackOption extends StatelessWidget {
-  final String optionText;
-
-  FeedbackOption(this.optionText);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(optionText),
-        Radio(value: null, groupValue: null, onChanged: null),
-      ],
     );
   }
 }
